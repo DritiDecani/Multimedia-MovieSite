a32832c8c152c6f4dedaa62131f0c65ba296ccb8
besmirgashi <bg38721@ubt-uni.net>
2022-06-14 01:05:26 +0200

    Video Play

--- Js/main.js
+++ Js/main.js
@@ -1,40 +1,47 @@
 //swiper
-      var swiper = new Swiper(".popular-content", {
-        slidesPerView:1,
-        spaceBetween:10,
-        autoplay: {
-          delay: 5500,
-          disableOnInteraction: false,
-        },
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-        breakpoints:{
-            280:{
-                slidesPerView:1,
-                spaceBetween:10,
-            },
-            320:{
-                slidesPerView:2,
-                spaceBetween:10,
-            },
-            510:{
-                slidesPerView:2,
-                spaceBetween:10,
-            },
-            750:{
-                slidesPerView:3,
-                spaceBetween:15,
-            },
-            900:{
-                slidesPerView:4,
-                spaceBetween:20,
-            },
+var swiper = new Swiper(".popular-content", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 5500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    280: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    510: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    750: {
+      slidesPerView: 3,
+      spaceBetween: 15,
+    },
+    900: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+  },
+});
+// Show video
+let playButton = document.querySelector('.play-movie');
+let video = document.querySelector('.video-container');
+let closebtn = document.querySelector('.close-video');
 
-        }
-      });
+playButton.onclick = () => {
+  video.classList.add('show-video');
+}
