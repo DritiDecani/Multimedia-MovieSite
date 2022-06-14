6edf63890cfc990accce7564cd6ed58e2ebb6db9
besmirgashi <bg38721@ubt-uni.net>
2022-06-14 14:59:01 +0200

    Movie Cast

--- Js/main.js
+++ Js/main.js
@@ -37,11 +37,20 @@ var swiper = new Swiper(".popular-content", {
     },
   },
 });
-// Show video
+// Show Video
 let playButton = document.querySelector('.play-movie');
 let video = document.querySelector('.video-container');
+let myvideo = document.querySelector('#myvideo');
 let closebtn = document.querySelector('.close-video');
 
 playButton.onclick = () => {
   video.classList.add('show-video');
+  // Auto Play When Click on Button
+  myvideo.play();
+}
+
+closebtn.onclick = () => {
+  video.classList.remove('show-video');
+  // Pause On Close Video
+  myvideo.pause();
 }
